2d6d147d16ba8351cc9574d5303ab0baa061a8a8
Kelly ONeil <you@example.com>
2016-09-30 17:27:13 -0500

    create header animation

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+// $('.transform').prepend('<div class="block" id="transform"></div>');
+
 //nav dropdown
 $('.dropdown').on('click', function () {
   $('.dropdown-links').slideToggle();
@@ -10,4 +13,33 @@ $('#contactForm').on('submit', function(event){
 $('#contactForm').find('input[type=text],[type=email]').val('');
 })
 
+headerMove();
 });
+
+//header movement
+var open;
+
+function headerMove() {
+  open = setTimeout(blockOpen, 2000);
+}
+
+function blockOpen() {
+
+  $('.transform').css("background-color", "darkolivegreen");
+  $('.transform').animate({marginLeft:'auto'}, {duration: 5000});
+// wordMove();
+$('h1').slideToggle();
+//maybe I could select each letter???
+
+// $('h1').animate({width: 'toggle'}, 3000);
+// $('h1').show('slide', {direction: 'left'}, 2000);
+}
+// var header;
+//
+// function wordMove() {
+//   header = setTimeout(title, 2000);
+// }
+//
+// function title() {
+//   $('h1').animate({width: 'toggle'});
+// }
